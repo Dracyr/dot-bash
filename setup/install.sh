35556b03d15c79315029696bdf625afe0531b5ea
Dracyr <dracyr@gmail.com>
2014-09-25 14:27:02 +0200

    Fix installing powerline fonts

--- setup/install.sh
+++ setup/install.sh
@@ -41,8 +41,9 @@ __install-terminal-fonts() {
   wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf
   wget https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
   mkdir -p ~/.fonts/
-  mkdir -p ~/.config/fontconfig/conf.d/
   mv PowerlineSymbols.otf ~/.fonts/
+  fc-cache -vf ~/.fonts/
+  mkdir -p ~/.config/fontconfig/conf.d/
   mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
 }
 
